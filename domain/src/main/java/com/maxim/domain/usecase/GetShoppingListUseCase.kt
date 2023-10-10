3f07d593f6df01c7e18b60950ca08aa4e029c771
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-10 19:07:50 +0300

    add usecases

---
+++ domain/src/main/java/com/maxim/domain/usecase/GetShoppingListUseCase.kt
@@ -0,0 +1,7 @@
+package com.maxim.domain.usecase
+
+import com.maxim.domain.model.ShoppingList
+
+class GetShoppingListUseCase {
+
+}
---
+++ domain/src/main/java/com/maxim/domain/usecase/RemoveItemUseCase.kt
@@ -0,0 +1,4 @@
+package com.maxim.domain.usecase
+
+class RemoveItemUseCase {
+}
