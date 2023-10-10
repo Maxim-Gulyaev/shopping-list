58acc2b030b355767f4e117fb5c1c5b0bd509c5b
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-10 23:20:00 +0300

    implement get usecase

--- domain/src/main/java/com/maxim/domain/usecase/GetShoppingListUseCase.kt
+++ domain/src/main/java/com/maxim/domain/usecase/GetShoppingListUseCase.kt
@@ -1,7 +1,8 @@
 package com.maxim.domain.usecase
 
-import com.maxim.domain.model.ShoppingList
+import com.maxim.domain.model.ShoppingListName
+import com.maxim.domain.repository.Repository
 
-class GetShoppingListUseCase {
-
+class GetShoppingListUseCase(private val repository: Repository) {
+    fun execute(shoppingListName: ShoppingListName) = repository.getShoppingList(shoppingListName)
 }
