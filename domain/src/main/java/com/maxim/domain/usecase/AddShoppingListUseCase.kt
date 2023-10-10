48191a30229e38e7b8932a32be622dff4abc2341
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-10 19:41:24 +0300

    add method in add usecase

--- domain/src/main/java/com/maxim/domain/usecase/AddShoppingListUseCase.kt
+++ domain/src/main/java/com/maxim/domain/usecase/AddShoppingListUseCase.kt
@@ -1,4 +1,8 @@
 package com.maxim.domain.usecase
 
+import com.maxim.domain.model.ShoppingList
+import com.maxim.domain.model.ShoppingListName
+
 class AddShoppingListUseCase {
+    fun execute(shoppingListName: ShoppingListName): ShoppingList = ShoppingList(shoppingListName)
 }
