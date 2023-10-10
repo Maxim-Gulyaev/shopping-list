6ebcb90b2b332d618db6e16f9a0e81203ae79fe3
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-10 23:34:34 +0300

    implement remove usecase

--- domain/src/main/java/com/maxim/domain/usecase/RemoveItemUseCase.kt
+++ domain/src/main/java/com/maxim/domain/usecase/RemoveItemUseCase.kt
@@ -1,4 +1,9 @@
 package com.maxim.domain.usecase
 
+import com.maxim.domain.model.Item
+import com.maxim.domain.model.ShoppingList
+
 class RemoveItemUseCase {
+    fun execute(item: Item, shoppingList: ShoppingList): Boolean =
+        shoppingList.shoppingList.remove(item)
 }
