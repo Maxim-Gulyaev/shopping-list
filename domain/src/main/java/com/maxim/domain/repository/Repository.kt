d1c2bceb9d3681cd5074c7f3e930eb3c392db16b
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-10 23:15:31 +0300

    implement repository methods

--- domain/src/main/java/com/maxim/domain/repository/Repository.kt
+++ domain/src/main/java/com/maxim/domain/repository/Repository.kt
@@ -1,4 +1,8 @@
 package com.maxim.domain.repository
 
+import com.maxim.domain.model.ShoppingListName
+
 interface Repository {
+    fun getShoppingList(shoppingListName: ShoppingListName)
+    fun saveShoppingList(shoppingListName: ShoppingListName)
 }
