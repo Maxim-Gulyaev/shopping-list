4abf25d2c4456f89f483a477842924d45554c935
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-11 21:26:26 +0300

    refactor repository

--- domain/src/main/java/com/maxim/domain/repository/Repository.kt
+++ domain/src/main/java/com/maxim/domain/repository/Repository.kt
@@ -1,8 +1,9 @@
 package com.maxim.domain.repository
 
+import com.maxim.domain.model.ShoppingList
 import com.maxim.domain.model.ShoppingListName
 
 interface Repository {
-    fun getShoppingList(shoppingListName: ShoppingListName)
+    fun getShoppingList(shoppingListName: ShoppingListName): ShoppingList
     fun saveShoppingList(shoppingListName: ShoppingListName)
 }
