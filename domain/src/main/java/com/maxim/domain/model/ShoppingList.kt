93db3c9b090b17d7b8339ebecc3a0921ff88a848
MaximGuljaev <maximuspo4ta@gmail.com>
2023-10-11 15:21:51 +0300

    refactor shopping list class

--- domain/src/main/java/com/maxim/domain/model/ShoppingList.kt
+++ domain/src/main/java/com/maxim/domain/model/ShoppingList.kt
@@ -1,5 +1,5 @@
 package com.maxim.domain.model
 
 data class ShoppingList(val shoppingListName: ShoppingListName) {
-    val listOfShopping = mutableListOf<Item>()
+    val listOfShopping = mutableSetOf<Item>()
 }
